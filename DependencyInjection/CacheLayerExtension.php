3c5c048894ed93c2a59c115c011254458ea06e94
pas <skrip42@gmail.com>
2020-10-08 10:24:24 +0700

    add requirement

--- DependencyInjection/CacheLayerExtension.php
+++ DependencyInjection/CacheLayerExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-//use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Reference;
@@ -25,26 +24,4 @@ class CacheLayerExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
-
-    //public function process(ContainerBuilder $container)
-    //{
-        //$cachedService = $container->findTaggedServiceIds('skrip42.cache');
-        //foreach ($cachedService as $service => $value) {
-            //$defenition = $container->getDefinition($service);
-            //$attr = $defenition->getTag('skrip42.cache')[0];
-            //$arguments = $defenition->getArguments();
-            //$defenition->setFactory([
-                //new Reference(CacheLayerFactory::class),
-                //'create'
-            //]);
-            //$defenition->setArguments(
-                //[
-                    //$defenition->getClass(),
-                    //new Reference($attr['cache']),
-                    //$attr,
-                    //$arguments
-                //]
-            //);
-        //}
-    //}
 }
